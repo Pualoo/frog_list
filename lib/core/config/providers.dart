a1869624c0617b6706863afe5720cd48b6912297
Paulo Chirnev <phchirnev@hotmail.com>
2024-02-11 21:52:29 -0300

    feat: 🚀 First implementation of app Global Bloc Providers

---
+++ lib/core/config/providers.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+final List<BlocProvider> appGlobalBlocProviders = [
+//   BlocProvider<SplashscreenBloc>(
+//     create: (_) => getIt<SplashscreenBloc>(),
+//   ),
+];
