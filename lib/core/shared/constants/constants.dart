b5c2ce8c334502fdd8210f355a867f357955f6fc
Paulo Chirnev <phchirnev@hotmail.com>
2024-02-11 21:54:29 -0300

    feat: 🚀 Implement app constants

---
+++ lib/core/shared/constants/constants.dart
@@ -0,0 +1,11 @@
+// Error Message
+const String kCacheFailureMessage = 'Cache Failure';
+
+//SharedPreferences constants
+const String kDashboardList = 'DASHBOARD-LIST';
+
+//Others
+//App
+const String kAppName = 'FrogList';
+
+//Images tags
