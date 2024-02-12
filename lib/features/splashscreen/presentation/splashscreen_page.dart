9af82a91481984963b75329a93a7510fde668ac2
Paulo Chirnev <phchirnev@hotmail.com>
2024-02-11 21:52:15 -0300

    feat: 🚀 First implementation of splash screen

---
+++ lib/features/splashscreen/presentation/splashscreen_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SplashscreenPage extends StatelessWidget {
+  static const String route = '/';
+  const SplashscreenPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(),
+        ],
+      ),
+    );
+  }
+}
