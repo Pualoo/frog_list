238e249ddc7f2fd022c7dd715f738b54493d14bb
Paulo Chirnev <phchirnev@hotmail.com>
2024-02-13 19:29:56 -0300

    feat: 🚀 implement dashboard domain tests

--- test/features/dashboard/domain/use_cases/delete_list_test.dart
+++ test/features/dashboard/domain/use_cases/delete_list_test.dart
@@ -1,78 +1,77 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:frog_list/core/shared/error/failures.dart';
+import 'package:frog_list/core/shared/error/failures.dart';
 import 'package:frog_list/features/checklist/domain/entities/checklist_entity.dart';
 import 'package:frog_list/features/dashboard/domain/entities/dashboard_list_entity.dart';
-import 'package:frog_list/features/dashboard/domain/repositories/dashboard_repository.dart';
-import 'package:frog_list/features/dashboard/domain/use_cases/add_new_list.dart';
-import 'package:mockito/annotations.dart';
+import 'package:frog_list/features/dashboard/domain/use_cases/delete_list.dart';
+
 import 'package:mockito/mockito.dart';
 
 import 'add_new_list_test.mocks.dart';
 
-@GenerateMocks([DashboardRepository])
 void main() {
-  late AddNewList useCase;
+  late DeleteList useCase;
   late MockDashboardRepository mockDashboardRepository;
 
   setUp(() {
     mockDashboardRepository = MockDashboardRepository();
-    useCase = AddNewList(mockDashboardRepository);
+    useCase = DeleteList(mockDashboardRepository);
   });
 
   test(
-    'should add new list and return empty list of dashboard from the repository',
-    () async {
+    'should delete a list and return empty list of dashboard from the repository',
+        () async {
       // arrange
       const String tNameTest = 'Test';
       final tList = [
         const DashboardListEntity(
             name: tNameTest, checklistPropertiesEntityList: []),
       ];
-      when(mockDashboardRepository.addNewList(any))
+      when(mockDashboardRepository.deleteList(any))
           .thenAnswer((_) async => Right(tList));
       // act
-      final result = await useCase(const AddNewListParams(name: tNameTest));
+      final result = await useCase(const DeleteListParams(name: tNameTest));
       // assert
       expect(result, Right(tList));
-      verify(mockDashboardRepository.addNewList(tNameTest));
+      verify(mockDashboardRepository.deleteList(tNameTest));
       verifyNoMoreInteractions(mockDashboardRepository);
     },
   );
 
   test(
-    'should add new list and return all lists of dashboard from the repository',
+    'should delete a list and return all lists of dashboard from the repository',
         () async {
       // arrange
       const String tNameTest = 'Test';
       final checklistPropertiesEntityList = [const ChecklistPropertiesEntity(text: 'Test1', isChecked: false), const ChecklistPropertiesEntity(text: 'Test2', isChecked: false), const ChecklistPropertiesEntity(text: 'Test3', isChecked: false),];
       final tList = [
         DashboardListEntity(
-            name: tNameTest, checklistPropertiesEntityList: checklistPropertiesEntityList,),
+          name: tNameTest, checklistPropertiesEntityList: checklistPropertiesEntityList,),
       ];
-      when(mockDashboardRepository.addNewList(any))
+      when(mockDashboardRepository.deleteList(any))
           .thenAnswer((_) async => Right(tList));
       // act
-      final result = await useCase(const AddNewListParams(name: tNameTest));
+      final result = await useCase(const DeleteListParams(name: tNameTest));
       // assert
       expect(result, Right(tList));
-      verify(mockDashboardRepository.addNewList(tNameTest));
+      verify(mockDashboardRepository.deleteList(tNameTest));
       verifyNoMoreInteractions(mockDashboardRepository);
     },
   );
 
   test(
-    'should try add new list but return Cache Failure from the repository',
+    'should try delete list but return Cache Failure from the repository',
         () async {
       // arrange
       const String tNameTest = 'Test';
-      when(mockDashboardRepository.addNewList(any))
+      when(mockDashboardRepository.deleteList(any))
           .thenAnswer((_) async => Left(CacheFailure()));
       // act
-      final result = await useCase(const AddNewListParams(name: tNameTest));
+      final result = await useCase(const DeleteListParams(name: tNameTest));
       // assert
       expect(result, Left(CacheFailure()));
-      verify(mockDashboardRepository.addNewList(tNameTest));
+      verify(mockDashboardRepository.deleteList(tNameTest));
       verifyNoMoreInteractions(mockDashboardRepository);
     },
   );
--- test/features/dashboard/domain/use_cases/get_dashboard_lists.dart
+++ test/features/dashboard/domain/use_cases/get_dashboard_lists.dart
@@ -1,78 +1,76 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:frog_list/core/shared/error/failures.dart';
+import 'package:frog_list/core/shared/use_cases/use_case.dart';
 import 'package:frog_list/features/checklist/domain/entities/checklist_entity.dart';
 import 'package:frog_list/features/dashboard/domain/entities/dashboard_list_entity.dart';
-import 'package:frog_list/features/dashboard/domain/repositories/dashboard_repository.dart';
-import 'package:frog_list/features/dashboard/domain/use_cases/add_new_list.dart';
-import 'package:mockito/annotations.dart';
+import 'package:frog_list/features/dashboard/domain/use_cases/get_dashboard_lists.dart';
+
 import 'package:mockito/mockito.dart';
 
 import 'add_new_list_test.mocks.dart';
 
-@GenerateMocks([DashboardRepository])
 void main() {
-  late AddNewList useCase;
+  late GetDashboardLists useCase;
   late MockDashboardRepository mockDashboardRepository;
 
   setUp(() {
     mockDashboardRepository = MockDashboardRepository();
-    useCase = AddNewList(mockDashboardRepository);
+    useCase = GetDashboardLists(mockDashboardRepository);
   });
 
   test(
-    'should add new list and return empty list of dashboard from the repository',
-    () async {
+    'should get empty list of dashboard from the repository',
+        () async {
       // arrange
       const String tNameTest = 'Test';
       final tList = [
         const DashboardListEntity(
             name: tNameTest, checklistPropertiesEntityList: []),
       ];
-      when(mockDashboardRepository.addNewList(any))
-          .thenAnswer((_) async => Right(tList));
+      when(mockDashboardRepository.getDashboardLists())
+          .thenAnswer((_) => Right(tList));
       // act
-      final result = await useCase(const AddNewListParams(name: tNameTest));
+      final result = await useCase(NoParams());
       // assert
       expect(result, Right(tList));
-      verify(mockDashboardRepository.addNewList(tNameTest));
+      verify(mockDashboardRepository.getDashboardLists());
       verifyNoMoreInteractions(mockDashboardRepository);
     },
   );
 
   test(
-    'should add new list and return all lists of dashboard from the repository',
+    'should get all lists of dashboard from the repository',
         () async {
       // arrange
       const String tNameTest = 'Test';
       final checklistPropertiesEntityList = [const ChecklistPropertiesEntity(text: 'Test1', isChecked: false), const ChecklistPropertiesEntity(text: 'Test2', isChecked: false), const ChecklistPropertiesEntity(text: 'Test3', isChecked: false),];
       final tList = [
         DashboardListEntity(
-            name: tNameTest, checklistPropertiesEntityList: checklistPropertiesEntityList,),
+          name: tNameTest, checklistPropertiesEntityList: checklistPropertiesEntityList,),
       ];
-      when(mockDashboardRepository.addNewList(any))
-          .thenAnswer((_) async => Right(tList));
+      when(mockDashboardRepository.getDashboardLists())
+          .thenAnswer((_) => Right(tList));
       // act
-      final result = await useCase(const AddNewListParams(name: tNameTest));
+      final result = await useCase(NoParams());
       // assert
       expect(result, Right(tList));
-      verify(mockDashboardRepository.addNewList(tNameTest));
+      verify(mockDashboardRepository.getDashboardLists());
       verifyNoMoreInteractions(mockDashboardRepository);
     },
   );
 
   test(
-    'should try add new list but return Cache Failure from the repository',
+    'should try get list but return Cache Failure from the repository',
         () async {
       // arrange
-      const String tNameTest = 'Test';
-      when(mockDashboardRepository.addNewList(any))
-          .thenAnswer((_) async => Left(CacheFailure()));
+      when(mockDashboardRepository.getDashboardLists())
+          .thenAnswer((_) => Left(CacheFailure()));
       // act
-      final result = await useCase(const AddNewListParams(name: tNameTest));
+      final result = await useCase(NoParams());
       // assert
       expect(result, Left(CacheFailure()));
-      verify(mockDashboardRepository.addNewList(tNameTest));
+      verify(mockDashboardRepository.getDashboardLists());
       verifyNoMoreInteractions(mockDashboardRepository);
     },
   );
